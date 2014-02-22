85f64e119e682432cc88c628c7b2207ab729da81
DedoCibula <dedo.cibula@gmail.com>
2014-02-22 14:02:39 -0500

    changed myhack

--- js/myhack.js
+++ js/myhack.js
@@ -1,9 +1,18 @@
-window.onload = function() {
+function removeWarnings() {
 	var script = document.querySelectorAll('[data-signature]')[0];
 	while (script && script.nextElementSibling.nodeName != "SCRIPT") {
 		script.nextElementSibling.parentNode.removeChild(script.nextElementSibling);
 	}
+}
+
+function getWindowHeight() {
 	var jewels = document.getElementsByClassName('jewel');
 	if (jewels.length != 0)
 		chrome.runtime.sendMessage({ 'resizeWindow': jewels[0].offsetHeight });
-};
+}
+
+removeWarnings();
+
+setTimeout(function() {
+	getWindowHeight();
+}, 300);
