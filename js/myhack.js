a8651422be1a08efd0abbe29523d4074345049f6
DedoCibula <dedo.cibula@gmail.com>
2015-03-17 23:53:57 +0100

    injected css, added read buttons enabling, added highlight fixing

--- js/myhack.js
+++ js/myhack.js
@@ -17,7 +17,27 @@ function changeAllMessagesUrl() {
 		anchor.href = 'https://www.facebook.com/messages';
 }
 
+function enableReadButtons() {
+	var containers = document.getElementsByClassName('notification');
+	for (var i = 0; i < containers.length; i++)
+		containers[i].style.position = 'relative';
+
+	var readButtons = document.getElementsByClassName('notif_x_div');
+	for (var i = 0; i < readButtons.length; i++) {
+		readButtons[i].classList.add('notification-read-div');
+		readButtons[i].firstChild.className = 'notification-read-button';
+	}
+}
+
+function fixHighlight() {
+	var links = document.querySelectorAll('#jewelCase .jewelHighlightItem li a');
+	for (var i = 0; i < links.length; i++)
+		links[i].classList.add('link');
+}
+
 removeWarnings();
+enableReadButtons();
+fixHighlight();
 
 setTimeout(function() {
 	getWindowHeight();
