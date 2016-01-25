9ed77e893de1c551e5af6b7c68e056babf5631a0
DedoCibula <dedo.cibula@gmail.com>
2016-01-25 01:14:53 -0500

    added hack to obtain facebook notifications

--- Facebook/background.ts
+++ Facebook/background.ts
@@ -21,7 +21,7 @@
     }
 
     class Loader implements Api.ILoader {
-        private static get NOTIFICATION_URI(): string { return "/ajax/notifications/client/get.php"; }
+        private static get NOTIFICATION_URI(): string { return "/ajax/notifications/client/get.php?__pc=EXP1%3ADEFAULT"; }
 
         private baseUrl: string;
 
@@ -47,6 +47,7 @@
                     url: this.baseUrl + Loader.NOTIFICATION_URI,
                     method: "POST",
                     accepts: "*/*",
+                    dataType: "text",
                     data: `__a=1&fb_dtsg=${token}`
                 }).done((result: string) => {
                     resolve(this.parseNotifications(JSON.parse((result).match(/{.*}/)[0]).payload));
@@ -71,15 +72,42 @@
         }
     }
 
+    function registerListeners(settings: ISettings): void {
+        chrome.webRequest.onBeforeSendHeaders.addListener(details => {
+            var headers: chrome.webRequest.HttpHeader[] = details.requestHeaders;
+            var refererFound: boolean = false, originFound: boolean = false;
+
+            for (var i = 0; i < headers.length; i++) {
+                if (headers[i].name === "Referer") {
+                    headers[i].value = settings.baseUrl;
+                    refererFound = true;
+                }
+                if (headers[i].name === "Origin") {
+                    headers[i].value = settings.baseUrl;
+                    originFound = true;
+                }
+            }
+
+            if (!refererFound) headers.push({ name: "Referer", value: settings.baseUrl });
+            if (!originFound) headers.push({ name: "Origin", value: settings.baseUrl });
+
+            return { requestHeaders: headers };
+        }, { urls: ["<all_urls>"] }, ["requestHeaders", "blocking"]);
+    }
+
     window.onload = () => {
         var settings: ISettings = {
             baseUrl: "https://www.facebook.com",
             refreshInterval: 20 * 1000
         };
 
+        registerListeners(settings);
+
         var loader: Api.ILoader = new Loader(settings);
         loader.getStatusAsync().then(status => {
-            console.log(status);
+            loader.getNotificationsAsync(status.token).then(result => {
+                console.log(result);
+            });
         });
     };
 }
