7919f57b1415ffa6bfc0b3838933f3aa528dc06f
DedoCibula <dedo.cibula@gmail.com>
2016-01-26 02:27:57 -0500

    added initial notification parsing

--- Facebook/background.ts
+++ Facebook/background.ts
@@ -36,7 +36,7 @@
                     method: "GET",
                     accepts: "*/*"
                 }).done((result: string) => {
-                    resolve(this.parseStatus(result));
+                    resolve(this.parseStatus(result.replace(/<img\b[^>]*>/ig, "")));
                 });
             });
         }
@@ -48,7 +48,7 @@
                     method: "POST",
                     accepts: "*/*",
                     dataType: "text",
-                    data: `__a=1&fb_dtsg=${token}`
+                    data: { length: 7, __a: 1, fb_dtsg: token }
                 }).done((result: string) => {
                     resolve(this.parseNotifications(JSON.parse((result).match(/{.*}/)[0]).payload));
                 });
@@ -68,7 +68,70 @@
         }
 
         private parseNotifications(json: any): Entities.Notification[] {
-            return null;
+            return (<Array<any>>(json.nodes)).map(notification => {
+                var authors: Entities.Author[] = (<Array<any>>(notification.actors)).map(author => new Entities.Author(author.name, author.profile_picture.uri));
+                var type: Entities.Type = this.parseNotificationType(notification.notif_type);
+
+                var timestamp: string;
+                if (notification.timestamp.text && notification.timestamp.text.length !== 0) {
+                    timestamp = notification.timestamp.text;
+                } else {
+                    var minutes: number = (json.servertime - notification.timestamp.time) / 60;
+                    timestamp = (minutes < 60) ?
+                        `${Math.floor(minutes)} minute${Math.floor(minutes) !== 1 ? "s" : ""} ago` :
+                        `${Math.floor(minutes / 60)} hour${Math.floor(minutes / 60) !== 1 ? "s" : ""} ago`;
+                }
+
+                var attachment: string = (notification.attachments.length > 0 && notification.attachments[0].media) ?
+                    notification.attachments[0].media.image.uri : null;
+
+                return new Entities.Notification(notification.id, notification.title.text, authors, type, timestamp, notification.url, notification.icon.uri, attachment);
+            });
+        }
+
+        private parseNotificationType(type: string): Entities.Type {
+            // TODO sanitize
+            switch (type) {
+                case "group_activity":
+                    return Entities.Type.GroupActivity;
+                case "birthday_reminder":
+                    return Entities.Type.BirthdayReminder;
+                case "fbpage_fan_invite":
+                    return Entities.Type.PageFanInvite;
+                case "admin_plan_mall_activity":
+                    return Entities.Type.AdminPlanMallActivity;
+                case "event_comment_mention":
+                    return Entities.Type.EventCommentMention;
+                case "feed_comment":
+                    return Entities.Type.FeedComment;
+                case "like":
+                    return Entities.Type.Like;
+                case "like_tagged":
+                    return Entities.Type.LikeTagged;
+                case "login_alerts_new_device":
+                    return Entities.Type.LoginAlert;
+                case "mentions_comment":
+                    return Entities.Type.MentionsComment;
+                case "photo_tag":
+                    return Entities.Type.PhotoTag;
+                case "plan_user_invited":
+                    return Entities.Type.PlanUserInvited;
+                case "poke":
+                    return Entities.Type.Poke;
+                case "tagged_with_story":
+                    return Entities.Type.TaggedWithStory;
+                case "wall":
+                    return Entities.Type.Wall;
+                default:
+                    return null;
+            }
+        }
+
+        private retrieveTimestamp(text: string, timeInSeconds: number): string {
+            if (text && text.length !== 0)
+                return text;
+            var minutes: number = Math.floor(timeInSeconds / 60);
+            return (minutes < 60) ? `${Math.floor(minutes)} minutes ago` : `${Math.floor(minutes / 60)} hours ago`;
         }
     }
 
--- Facebook/entities.ts
+++ Facebook/entities.ts
@@ -1,11 +1,64 @@
 ﻿module Facebook.Entities {
     export class Status {
-        constructor(public token: string, public notificationCount: number, public messageCount: number) {
+        public token: string;
+        public notificationCount: number;
+        public messageCount: number;
+
+        constructor(token: string, notificationCount: number, messageCount: number) {
+            this.token = token;
+            this.notificationCount = notificationCount;
+            this.messageCount = messageCount;
+        }
+    }
+
+    export const enum Type {
+        GroupActivity,
+        BirthdayReminder,
+        FeedComment,
+        Like,
+        LikeTagged,
+        PageFanInvite,
+        PhotoTag,
+        Poke,
+        MentionsComment,
+        LoginAlert,
+        PlanUserInvited,
+        Wall,
+        EventCommentMention,
+        AdminPlanMallActivity,
+        TaggedWithStory
+    }
+
+    export class Author {
+        public name: string;
+        public profilePicture: string;
+
+        constructor(name: string, profilePicture: string) {
+            this.name = name;
+            this.profilePicture = profilePicture;
         }
     }
 
     export class Notification {
+        public id: string;
+        public text: string;
+        public authors: Author[];
+        public type: Type;
+        public timestamp: string;
+        public url: string;
+        public icon: string;
+        public attachment: string;
 
+        constructor(id: string, text: string, authors: Author[], type: Type, timestamp: string, url: string, icon: string, attachment?: string) {
+            this.id = id;
+            this.text = text;
+            this.authors = authors;
+            this.type = type;
+            this.timestamp = timestamp;
+            this.url = url;
+            this.icon = icon;
+            this.attachment = attachment;
+        }
     }
 
     export class Message {
