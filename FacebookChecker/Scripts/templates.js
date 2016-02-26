22e564db38174ee655a4aa458309b5df68282c29
DedoCibula <dedo.cibula@gmail.com>
2016-02-26 18:48:04 -0500

    added support for notification attachments

--- FacebookChecker/Scripts/templates.js
+++ FacebookChecker/Scripts/templates.js
@@ -28,7 +28,7 @@ templates['messages'] = template({"1":function(depth0,helpers,partials,data) {
   return ((stack1 = helpers.each.call(depth0,depth0,{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.program(3, data, 0),"data":data})) != null ? stack1 : "");
 },"useData":true});
 templates['notifications'] = template({"1":function(depth0,helpers,partials,data) {
-    var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
+    var stack1, helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
 
   return "<a id=\""
     + alias3(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"id","hash":{},"data":data}) : helper)))
@@ -38,20 +38,34 @@ templates['notifications'] = template({"1":function(depth0,helpers,partials,data
     + alias3((helpers.checkNew || (depth0 && depth0.checkNew) || alias1).call(depth0,(depth0 != null ? depth0.state : depth0),{"name":"checkNew","hash":{},"data":data}))
     + "\">\r\n    <div class=\"row\">\r\n        <div class=\"col-xs-2\">\r\n            "
     + alias3((helpers.renderPicture || (depth0 && depth0.renderPicture) || alias1).call(depth0,(depth0 != null ? depth0.authors : depth0),{"name":"renderPicture","hash":{},"data":data}))
-    + "\r\n        </div>\r\n        <div class=\"col-xs-10\">\r\n            <div class=\"primary-text\">\r\n                "
+    + "\r\n        </div>\r\n        \r\n        <div class=\""
+    + ((stack1 = helpers['if'].call(depth0,(depth0 != null ? depth0.attachment : depth0),{"name":"if","hash":{},"fn":this.program(2, data, 0),"inverse":this.program(4, data, 0),"data":data})) != null ? stack1 : "")
+    + "\">\r\n            <div class=\"primary-text\">\r\n                "
     + alias3((helpers.emphasize || (depth0 && depth0.emphasize) || alias1).call(depth0,depth0,{"name":"emphasize","hash":{},"data":data}))
     + "\r\n            </div>\r\n            <div class=\"secondary-text\">\r\n                <img class=\"icon\" src=\""
     + alias3(((helper = (helper = helpers.icon || (depth0 != null ? depth0.icon : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"icon","hash":{},"data":data}) : helper)))
     + "\" alt=\"\" />\r\n                <span>"
     + alias3(((helper = (helper = helpers.timestamp || (depth0 != null ? depth0.timestamp : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"timestamp","hash":{},"data":data}) : helper)))
-    + "</span>\r\n            </div>\r\n        </div>\r\n        <div class=\"read-button-container\">\r\n            <div title=\"Read\" class=\"read-button\" data-read-info=\""
+    + "</span>\r\n            </div>\r\n        </div>\r\n"
+    + ((stack1 = helpers['if'].call(depth0,(depth0 != null ? depth0.attachment : depth0),{"name":"if","hash":{},"fn":this.program(6, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
+    + "        <div class=\"read-button-container\">\r\n            <div title=\"Read\" class=\"read-button\" data-read-info=\""
     + alias3((helpers.serializeReadInfo || (depth0 && depth0.serializeReadInfo) || alias1).call(depth0,depth0,{"name":"serializeReadInfo","hash":{},"data":data}))
     + "\" style=\"display:none;\"></div>\r\n        </div>\r\n    </div>\r\n</a>\r\n";
-},"3":function(depth0,helpers,partials,data) {
+},"2":function(depth0,helpers,partials,data) {
+    return "col-xs-7";
+},"4":function(depth0,helpers,partials,data) {
+    return "col-xs-10";
+},"6":function(depth0,helpers,partials,data) {
+    var helper;
+
+  return "        <div class=\"col-xs-3\">\r\n            <img src=\""
+    + this.escapeExpression(((helper = (helper = helpers.attachment || (depth0 != null ? depth0.attachment : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"attachment","hash":{},"data":data}) : helper)))
+    + "\" class=\"attachment\" alt=\"\" />\r\n        </div>\r\n";
+},"8":function(depth0,helpers,partials,data) {
     return "<div class=\"row list-group-item\">\r\n    <div class=\"col-xs-12\">\r\n        <span class=\"center-block\">No notifications found</span>\r\n    </div>\r\n</div>\r\n";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var stack1;
 
-  return ((stack1 = helpers.each.call(depth0,depth0,{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.program(3, data, 0),"data":data})) != null ? stack1 : "");
+  return ((stack1 = helpers.each.call(depth0,depth0,{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.program(8, data, 0),"data":data})) != null ? stack1 : "");
 },"useData":true});
 })();
--- FacebookChecker/background.ts
+++ FacebookChecker/background.ts
@@ -397,8 +397,9 @@
                 const authors: Entities.Author[] = (notification.actors as Array<any>).map(author => new Entities.Author(author.name, author.profile_picture.uri, author.name.split(" ")[0]));
                 const state: Entities.State = this.parseState(notification.seen_state as string);
                 const timestamp: string = this.formTimestampText(notification.timestamp.text, json.servertime - notification.timestamp.time);
-                const attachment: string = (notification.attachments.length > 0 && notification.attachments[0].media) ?
-                    notification.attachments[0].media.image.uri : null;
+                const attachment: string = (notification.attached_story && notification.attached_story.attachments && notification.attached_story.attachments[0] &&
+                    notification.attached_story.attachments[0].media && notification.attached_story.attachments[0].media.image) ?
+                    notification.attached_story.attachments[0].media.image.uri : null;
 
                 return new Entities.Notification(notification.id, notification.title.text, emphases, authors, authors[0].profilePicture, state, notification.alert_id.split(":")[1], timestamp, notification.url, notification.icon.uri, attachment);
             });
